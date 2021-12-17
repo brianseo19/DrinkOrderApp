b9717db77b7fc267bd67dbc8778264314d045e88
brianseo19 <brian.seo919@gmail.com>
2021-12-16 17:51:22 -0700

    Initial Commit

---
+++ DrinkOrderApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrinkOrderApp
+//
+//  Created by Brian Seo on 12/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrinkOrderApp/DrinkOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkOrderAppApp.swift
+//  DrinkOrderApp
+//
+//  Created by Brian Seo on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct DrinkOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
